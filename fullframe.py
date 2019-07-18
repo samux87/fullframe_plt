a528009c6fd0ce37f999cfdee50d8323f2d15b30
samux87 <samux87@users.noreply.github.com>
2019-07-18 10:17:41 +0100

    init

---
+++ fullframe.py
@@ -0,0 +1,10 @@
+def full_frame(width=None, height=None):
+    import matplotlib as mpl
+    mpl.rcParams['savefig.pad_inches'] = 0
+    figsize = None if width is None else (width, height)
+    fig = plt.figure(figsize=figsize)
+    ax = plt.axes([0,0,1,1], frameon=False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.autoscale(tight=True)
+    
